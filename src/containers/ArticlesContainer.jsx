58c4b892bac8e76dd710536cd857c686b653c43e
Jake Thrasher <mjakethrasher@gmail.com>
2021-05-12 16:42:54 -0700

    initialize container and test

---
+++ src/containers/ArticlesContainer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ArticlesContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
+}
---
+++ src/containers/ArticlesContainer.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import ArticlesContainer from './ArticlesContainer'
+
+describe('ArticlesContainer', ()=>{
+    it('should render a list of articles', async()=>{
+        render(<ArticlesContainer/>)
+
+        screen.getByText('Loading...')
+    })
+})
